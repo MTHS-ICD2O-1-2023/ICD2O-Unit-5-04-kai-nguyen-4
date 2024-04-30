204e34e00afc0feec5e84b8cf89b80cbf62981e6
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-04-30 18:41:42 +0000

    created discount form rough draft

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function findDiscountClicked() {
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  const currentDate = (document.getElementById("day-entered").value)
+
+  if (((userAge > 12) && (userAge < 21)) || ((currentDate == "Tuesday") || (currentDate == "Thursday"))) {
+    document.getElementById("answer").innerHTML = "You are eligible for the discount"
+  } else {
+    document.getElementById("answer").innerHTML = "You are Not eligible for the discount"
+  }
+
+  // Ouput the answer to the user in the class "answer"
+}
